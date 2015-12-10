081bb18e35274196d75f077014eaa5571c455c03
Mramaizng <beersbr@gmail.com>
2015-12-10 02:16:18 -0800

    added keyboard and more matrix

--- bretter.js
+++ bretter.js
@@ -1,3 +1,133 @@
+function bind(scope, fn)
+{
+    return (function(){
+        fn.apply(scope, arguments);
+    });
+}
+
+function MouseHandler(el){
+    this.keys = {};
+    this.mouseDown = {x: undefined, y: undefined};
+    this.mousePos = {x: undefined, y: undefined};
+
+    this.mouseClick = undefined;
+
+    this.clicked = function(){
+        var click = this.mouseClick;
+        this.mouseClick = undefined;
+        return click;
+    }
+
+    this.MouseUp = function(event)
+    {
+        // event.stopPropagation();
+        // event.preventDefault();
+        // console.log("mouseup");
+    }
+
+    this.MouseDown = function(event)
+    {
+        event.stopPropagation();
+        event.preventDefault();
+
+        this.mouseDown.x = event.offsetX;
+        this.mouseDown.y = event.offsetY;
+        // console.log("mousedown");
+    }
+
+    this.MouseMove = function(e)
+    {
+        this.mousePos.x = e.x;
+        this.mousePos.y = e.y;
+    }
+
+    this.MouseClick = function(e)
+    {
+        e.stopPropagation();
+        e.preventDefault();
+
+        this.mouseClick = {};
+        this.mouseClick.x = e.offsetX;
+        this.mouseClick.y = e.offsetY;
+    }
+
+    // attach touch listeners
+    el.addEventListener("touchstart", bind(this, this.MouseUp));
+    el.addEventListener("touchend", bind(this, this.MouseDown));
+
+    // Attach mouse listeners
+    el.addEventListener("mouseup", bind(this, this.MouseUp));
+    el.addEventListener("mousedown", bind(this, this.MouseDown));
+    el.addEventListener("mousemove", bind(this, this.MouseMove));
+    el.addEventListener("click", bind(this, this.MouseClick));
+
+    // Attach keyboard listeners
+    // window.addEventListener("keyup", bind(this, this.KeyUp));
+    // window.addEventListener("keydown", bind(this, this.KeyDown));
+}
+
+function KeyboardHandler()
+{
+    this.setKey = function(key)
+    {
+        // If we are setting the key for the first time make sure it has a value
+        if(this.keyStates[key.keyCode] == null) this.keyStates[key.keyCode] = false;
+        this.keyStates[key.keyCode] = true;
+        return true;
+    }
+
+    // unsets the key in the buffer variable
+    this.unsetKey = function(key)
+    {
+        this.keyStates[key.keyCode] = false;
+        this.pressed_keys[key.keyCode] = true;
+
+        return true;
+    }
+
+    // This is just a getter to make the later syntax easier to read through
+    this.key = function(key_value)
+    {
+        if(this.keyStates[key_value]) return true;
+        return false;
+    }
+
+    this.keyIsDown = function(letter)
+    {
+        if(this.keyStates[keyHash[letter]]) return true;
+        return false;
+    }
+
+    this.keyPressed = function(letter)
+    {
+        return this.pressed_keys[keyHash[letter]];
+    }
+
+    this.setKeyPressed = function(letter)
+    {
+        // console.log(letter);
+    }
+
+    this.cleaner = function()
+    {
+        this.pressed_keys = {};
+    }
+
+    this.listenOffsetX = 0;
+    this.listenOffsetY = 0;
+    this.mx = 0;
+    this.my = 0;
+
+    this.pressed_keys = {};
+    this.keyStates = {};
+
+    var keyHash = {'backspace': 8,'tab': 9,'enter': 13,'shift': 16,'ctrl': 17,'alt': 18,'pause_break': 19,'caps_lock': 20,'escape': 27,'page_up': 33,'page_down': 34,'end': 35,'home': 36,'left_arrow': 37,'up_arrow': 38,'right_arrow': 39,'down_arrow': 40,'insert': 45,'delete': 46,'0': 48,'1': 49,'2': 50,'3': 51,'4': 52,'5': 53,'6': 54,'7': 55,'8': 56,'9': 57,'a': 65,'b': 66,'c': 67,'d': 68,'e': 69,'f': 70,'g': 71,'h': 72,'i': 73,'j': 74,'k': 75,'l': 76,'m': 77,'n': 78,'o': 79,'p': 80,'q': 81,'r': 82,'s': 83,'t': 84,'u': 85,'v': 86,'w': 87,'x': 88,'y': 89,'z': 90,'left_window_key': 91,'right_window_key': 92,'select_key': 93,'numpad_0': 96,'numpad_1': 97,'numpad_2': 98,'numpad_3': 99,'numpad_4': 100,'numpad_5': 101,'numpad_6': 102,'numpad_7': 103,'numpad_8': 104,'numpad_9': 105,'multiply': 106,'add': 107,'subtract': 109,'decimal_point': 110,'divide': 111,'f1': 112,'f2': 113,'f3': 114,'f4': 115,'f5': 116,'f6': 117,'f7': 118,'f8': 119,'f9': 120,'f10': 121,'f11': 122,'f12': 123,'num_lock': 144,'scroll_lock': 145,'semi_colon': 186,'equal_sign': 187,'comma': 188,'dash': 189,'period': 190,'forward_slash': 191,'grave_accent': 192,'open_bracket': 219,'back_slash': 220,'close_braket': 221,'single_quote': 222};
+    window.addEventListener("keydown", bind(this, this.setKey), false);
+    window.addEventListener("keyup", bind(this, this.unsetKey), false);
+    window.addEventListener("keypress", bind(this, this.setKeyPressed), false);
+
+}
+
 var AnimationEasingFunctions = (function(){
 	/**
 	 * Easing functions to be used by the animation engine
@@ -318,6 +448,8 @@ Vector3.unit = function(A) {
 // }
 
 
+// NOTE(brett): These are copies of glMatrix.js
+
 var Matrix4 = {};
 Matrix4.create = function() {
 	return ([1, 0, 0, 0,
@@ -341,6 +473,28 @@ Matrix4.perspective = function(fovy, aspect, near, far) {
 			 0, 0, (far+near)*r, 1,
 			 0, 0, (2.0*far*near)*r, 0]);
 }
+
+Matrix4.scale = function(m, v3) {
+	d = [];
+	d[0] = m[0]*v3.x;
+	d[1] = m[1]*v3.x; 
+	d[2] = m[2]*v3.x;
+	d[3] = m[3]*v3.x;
+	d[4] = m[4]*v3.y;
+	d[5] = m[5]*v3.y;
+	d[6] = m[6]*v3.y;
+	d[7] = m[7]*v3.y;
+	d[8] = m[8]*v3.z;
+	d[9] = m[9]*v3.z;
+	d[10] = m[10]*v3.z;
+	d[11] = m[11]*v3.z;
+	d[12] = m[12];
+	d[13] = m[13];
+	d[14] = m[14];
+	d[15] = m[15];
+	return d;
+}
+
 
 
 function ProjectPolygonOnAxis2D(polygon, axis)
@@ -395,6 +549,19 @@ function TriangulateConvexPolygon(points)
 	}
 
 	return triangles;
+}
+
+function PolygonBoundingBox(points) {
+	var xs = points.map(function(c) { return c.x });
+	var ys = points.map(function(c) { return c.x });
+
+	var minx = Math.min.apply(null, xs);
+	var miny = Math.min.apply(null, ys);
+	var maxx = Math.max.apply(null, xs);
+	var maxy = Math.max.apply(null, ys);
+
+	return [minx, miny, maxx-minx, maxy-miny];
+
 }
 
 function SATCollision(polygonA, polygonB)
@@ -547,14 +714,14 @@ function CreateShaderProgram(vertex_source, fragment_source, attributes, uniform
 	shaderProgram.attributes = {};
 
 	for(var attr_index in attributes) {
-		shaderProgram.attributes[attributes[parseInt(attr_index)]] = gl.getAttribLocation(shaderProgram.item, attributes[parseInt(attr_index)]);
-		gl.enableVertexAttribArray(shaderProgram.attributes[attributes[parseInt(attr_index)]]);
+		shaderProgram.attributes[attributes[parseInt(attr_index)]] = gl.getAttribLocation(shaderProgram, attributes[parseInt(attr_index)]);
+		// gl.enableVertexAttribArray(shaderProgram.attributes[attributes[parseInt(attr_index)]]);
 	}
 
 	for(var uni_index in uniforms) {
-		shaderProgram.uniforms[uniforms[parseInt(uni_index)]] = gl.getUniformLocation(shaderProgram.item, uniforms[parseInt(uni_index)]);
+		shaderProgram.uniforms[uniforms[parseInt(uni_index)]] = gl.getUniformLocation(shaderProgram, uniforms[parseInt(uni_index)]);
 	}
 
-	return true;
+	return shaderProgram;
 }
 
